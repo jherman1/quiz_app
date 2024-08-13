ac3375826d2b36ac270ab27fabe8a8ca6cdd4aee
Jake Herman <jake@Jakes-MacBook-Pro-2.local>
2024-08-13 13:42:32 -0600

    cleaned up questions_summary.dart to use new QuestionsSummaryRow widget

---
+++ lib/questions_summary_row.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummaryRow extends StatelessWidget {
+  const QuestionsSummaryRow({super.key, required this.data});
+
+  final Map<String, Object> data;
+
+  final Color correctColor = const Color.fromARGB(148, 121, 227, 125);
+  final Color incorrectColor = const Color.fromARGB(167, 240, 60, 81);
+
+  @override
+  Widget build(BuildContext context) {
+
+    bool wasCorrect = data['user_answer'] == data['correct_answer'];
+
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            color: wasCorrect ? correctColor : incorrectColor,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: Text(
+              ((data['question_index'] as int) + 1).toString(),
+              style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data['question'] as String,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  data['user_answer'] as String,
+                  style: TextStyle(
+                    color: wasCorrect ? correctColor : incorrectColor,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  data['correct_answer'] as String,
+                  style: TextStyle(
+                    color: correctColor,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
